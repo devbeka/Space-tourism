4040c8a17a06d983a2b43179af68a86ea788d3a8
xkkaaaa <cholponbaevb655@gmail.com>
2022-04-25 19:03:30 +0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+$('.slider__box').slick({
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="./images/Arrow-left.svg" alt="left">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="./images/Arrow-right.svg" alt="right">',
+});
+});
